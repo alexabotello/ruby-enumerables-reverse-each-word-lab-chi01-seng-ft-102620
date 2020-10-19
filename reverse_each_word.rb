52846a06923cd3254fb920108d767894b49ab61d
Alexa Botello <abotello20@yahoo.com>
2020-10-19 23:36:17 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+  returnedArray = []
+  newArray = string.split(" ")
+  newArray = newArray.reverse
+  newArray.collect do |word|
+    returnedArray << word.reverse
+  end
+  newString = returnedArray.join(" ").to_s
+  puts "newString = #{newString}"
+end
